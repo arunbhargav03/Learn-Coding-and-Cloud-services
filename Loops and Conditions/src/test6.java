1b9718eb608369174eb2d158f75c948af1259558
Naresh <nareshchallala574@gmail.com>
2023-04-18 18:12:39 +0100

    Assignement 2 Loops

---
+++ Loops and Conditions/src/test6.java
@@ -0,0 +1,26 @@
+public class test6 {
+    public static void main(String[] args){
+        int name = 4;
+        switch (name){
+            case 1:
+                System.out.println("Arun");
+                break;
+            case 2:
+                System.out.println("Dinesh");
+                break;
+            case 3:
+                System.out.println("Varun");
+                break;
+            case 4:
+                System.out.println("Jitu");
+                break;
+            case 5:
+                System.out.println("Naresh");
+                break;
+            case 6:
+                System.out.println("Gourav");
+                break;
+        }
+
+    }
+}
